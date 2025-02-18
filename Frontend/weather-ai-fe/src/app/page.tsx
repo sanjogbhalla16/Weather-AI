e033678391900a25b0c73cf47cff6b6d066e645b
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2025-02-18 19:28:55 +0530

    Changes in Frontend

--- Frontend/weather-ai-fe/src/app/page.tsx
+++ Frontend/weather-ai-fe/src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/mode-toggle"; // Import the toggle button
 import { Input } from "@/components/ui/input";
