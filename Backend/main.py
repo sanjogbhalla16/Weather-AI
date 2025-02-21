1ca3a91a5c702cbd79cbefa64ded8a0af47bbfe9
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2025-02-21 09:55:07 +0530

    Changes in Backend

--- Backend/main.py
+++ Backend/main.py
@@ -23,13 +23,13 @@ logfire.configure()
 app = FastAPI()
 
 #we have a frontend origin which is localhost:3000
-origins = [
-    "http://localhost:3000"
-]
+# origins = [
+#     "http://localhost:3000"
+# ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
